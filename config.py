3f3c8b27113be24281340dd582a136b8d0490106
a73garcia <153382653+a73garcia@users.noreply.github.com>
2026-07-17 17:20:43 +0200

    Add initial configuration module

---
+++ config.py
@@ -0,0 +1,12 @@
+"""Configuración de Wiki Local"""
+
+HOST = "127.0.0.1"
+PORT = 8080
+DEBUG = True
+AUTO_OPEN_BROWSER = True
+DATABASE = "database/wiki.db"
+PAGES_DIR = "pages"
+ATTACHMENTS_DIR = "attachments"
+BACKUPS_DIR = "backups"
+LOGS_DIR = "logs"
+LOG_LEVEL = "INFO"
